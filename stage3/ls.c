7e1c3c68cea38506b2627e2eab86d7c838d91b60
Adam Morawiec <amrw+gitlab@pm.me>
2018-12-20 14:37:47 +0100

    Refactor ls.c

--- stage3/ls.c
+++ stage3/ls.c
@@ -55,8 +55,53 @@ struct Dir openDirectory(int argc, char *argv[])
   }
 
   struct Dir directory = {dirDescriptor, flag};
-
   return directory;
+}
+
+void printDirEntry(char *entryName, char *ext, int flag)
+{
+  int i;
+
+  if (ext[0] == 0 || ext[0] == '.' || ext[0] == ' ')
+  {
+    // If it's a directory, append slash to the name.
+    if (entryName[0] != '.')
+    {
+      for (i = 0; i < strlen(entryName); i++)
+      {
+        if (entryName[i] == ' ' || entryName[i] == 0)
+        {
+          entryName[i] = '/';
+          break;
+        }
+      }
+    }
+    printf("%s\n", entryName);
+
+    if (flag == 1)
+    {
+      printf("Flag == \"-l\"\n");
+    }
+  }
+  else
+  {
+    // If the entry has an extension, append it to the name.
+    for (i = 0; i < strlen(entryName); i++)
+    {
+      if (entryName[i] == ' ' || entryName[i] == 0)
+      {
+        entryName[i] = '.';
+        memmove(entryName + i + 1, ext, strlen(ext));
+        break;
+      }
+    }
+    printf("%s\n", entryName);
+
+    if (flag == 1)
+    {
+      printf("Flag == \"-l\"\n");
+    }
+  }
 }
 
 int main(int argc, char *argv[])
@@ -71,7 +116,6 @@ int main(int argc, char *argv[])
 
   struct _DirectoryEntry *dirEntry = malloc(32);
   char entryName[12] = {0}, ext[4] = {0};
-  int i;
 
   for (;;)
   {
@@ -84,46 +128,7 @@ int main(int argc, char *argv[])
       memmove(entryName, &dirEntry->Filename, 8);
       memmove(ext, &dirEntry->Ext, 3);
 
-      if (ext[0] == 0 || ext[0] == '.' || ext[0] == ' ')
-      {
-        // If it's a directory, append slash to the name.
-        if (entryName[0] != '.')
-        {
-          for (i = 0; i < strlen(entryName); i++)
-          {
-            if (entryName[i] == ' ' || entryName[i] == 0)
-            {
-              entryName[i] = '/';
-              break;
-            }
-          }
-        }
-        printf("%s\n", entryName);
-
-        if (directory.flag == 1)
-        {
-          printf("Flag == \"-l\"\n");
-        }
-      }
-      else
-      {
-        // If the entry has an extension, append it to the name.
-        for (i = 0; i < strlen(entryName); i++)
-        {
-          if (entryName[i] == ' ' || entryName[i] == 0)
-          {
-            entryName[i] = '.';
-            memmove(entryName + i + 1, ext, strlen(ext));
-            break;
-          }
-        }
-        printf("%s\n", entryName);
-
-        if (directory.flag == 1)
-        {
-          printf("Flag == \"-l\"\n");
-        }
-      }
+      printDirEntry(entryName, ext, directory.flag);
     }
     else
     {
